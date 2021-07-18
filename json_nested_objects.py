0fb042bc8c426f468765f299ae638ca93de9ca89
Benjamin Saavedra <b3nja16@protonmail.com>
2021-07-18 18:44:01 -0400

    .

--- json_nested_objects.py
+++ json_nested_objects.py
@@ -1,13 +1,13 @@
 import json
 
 config = {
-    "Token": "ODMyNDAyMDg4NDExMzk4MTg2.YHjQ2w.1Sx4E_Q-NMhf_uuUcPyC4FKPT5Q",
-    "GuildID": 661257524653195289,
-    "ModID": 687376984443060229,
-    "MemberID": 700463151178711131,
+    "Token": "",
+    "GuildID": ,
+    "ModID": ,
+    "MemberID": ,
     "Toke": {
-            "T": 861996789791850496,
-            "A": 8619967
+            "T": ,
+            "A": 
         }   
 }
 
